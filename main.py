56f46e0a16eba8518a0ed9b9f7dd431ec08c5466
Vinicius Oliveira <vinicius_oc09@hotmail.com>
2022-07-18 16:38:01 -0300

    Fix Length condition

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from verifications import verification_case, validation_cpf
 from string_processing import cpf
 
-while(True):
-    if len(cpf) != 11:
-        print("Tamanho inválido")
-        break
 
+if len(cpf) != 11:
+    print("Tamanho inválido")
+else:
     if verification_case() and validation_cpf():
         print("CPF VÁLIDO")
     else:
         print("CPF INVÁLIDO")
-
-    input("Pressione ENTER para sair...")
+    
+input("Pressione ENTER para sair...")
     
 
