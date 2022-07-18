cbad2db510097db1dcbf1c2979f3006d3c554657
Vinicius Oliveira <vinicius_oc09@hotmail.com>
2022-07-18 13:14:13 -0300

    Optimize string_processing

--- string_processing.py
+++ string_processing.py
@@ -1,25 +1,20 @@
 cpf = input("Digite um CPF [xxx.xxx.xxx-xx]: ")
 
 def string_processor():
-    treated_cpf = ""
 
-    digit_list = cpf[0:11].split('.')
-    treated_cpf = "".join(digit_list)
+    digit_list = cpf[0:11].replace('.', '')
+    treated_cpf = (digit_list)
 
     return treated_cpf
 
 def complete_string_processor():
-    treated_cpf = ""
 
-    digit_list = cpf.split('.')
-    treated_cpf = "".join(digit_list)
-    digit_list = treated_cpf.split('-')
-    treated_cpf = "".join(digit_list)
+    digit_list = cpf.replace('.', '')
+    digit_list = digit_list.replace('-', '')
+    
+    treated_cpf = digit_list
 
-
-    if treated_cpf[0] == treated_cpf[1] and treated_cpf[1] == treated_cpf[2] and treated_cpf[2] == treated_cpf[3] and treated_cpf[3] == treated_cpf[4]\
-    and treated_cpf[4] == treated_cpf[5] and treated_cpf[5] == treated_cpf[6] and treated_cpf[6] == treated_cpf[7] and treated_cpf[7] == treated_cpf[8]\
-    and treated_cpf[8] == treated_cpf[9] and treated_cpf[10]:
+    if treated_cpf == treated_cpf[0] * 11:
         return False
 
     return treated_cpf
